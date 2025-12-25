3ffbc5d0948dc599c225b343b5c10c10fa75020c
Anthony Desert <contact@anthonydesert.fr>
2025-12-25 21:40:25 +0100

    Ajout de Bitwarden client

--- debian-config.sh
+++ debian-config.sh
@@ -1,3 +1,6 @@
+#Script de configuration Debian pour workstation Anthony Désert
+
+#Windsurf
 sudo apt-get install wget gpg
 wget -qO- "https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/windsurf.gpg" | gpg --dearmor > windsurf-stable.gpg
 sudo install -D -o root -g root -m 644 windsurf-stable.gpg /etc/apt/keyrings/windsurf-stable.gpg
@@ -6,5 +9,15 @@ rm -f windsurf-stable.gpg
 sudo apt install apt-transport-https
 sudo apt update
 sudo apt install windsurf -y
+
+#Nextcloud
 sudo apt install nextcloud-desktop -y
 
+#Bitwarden client
+sudo apt install -y curl gpg apt-transport-https
+curl -fsSL https://downloads.bitwarden.com/linux/debian/bitwarden.gpg | \
+sudo gpg --dearmor -o /usr/share/keyrings/bitwarden.gpg
+echo "deb [signed-by=/usr/share/keyrings/bitwarden.gpg] https://downloads.bitwarden.com/linux/debian stable main" | \
+sudo tee /etc/apt/sources.list.d/bitwarden.list
+sudo apt update
+sudo apt install bitwarden
